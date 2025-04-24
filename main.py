c6a22b8b3bdfe15dea3ddf53971751bb7653bb55
Marek Svoboda <svobodam.2002@pm.me>
2025-04-24 18:30:47 +0200

    Refactored main function

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def main():
-    print(get_book_text("books/frankenstein.txt"))
+from stats import get_words_count
 
-def get_book_text(filepath):
-    with open(filepath, encoding="utf8") as f:
+def main():
+    with open("books/frankenstein.txt") as f:
         file_content = f.read()
-    return file_content
+    words_count = get_words_count(file_content)
+    print(f"{words_count} words found in the document")
 
 main()
