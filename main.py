f830dcdaff549b290ab83cfa9f4ed7b3b2479cd2
ValFan771 <svobodam.2002@pm.me>
2025-04-25 23:16:56 +0200

    Completed sort_dict function and added option to import any .txt file

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from stats import *
+import sys
 
 def main():
-    with open("books/frankenstein.txt", encoding="UTF-8") as f:
+    with open(sys.argv[1], encoding="UTF-8") as f:
         file_content = f.read()
     words_count = get_words_count(file_content)
     char_count = get_char_count(file_content)
-    print(f"{words_count} words found in the document")
-    print(sort_dict(char_count))
+    
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}...")
+    print("----------- Word Count ----------")
+    print(f"Found {words_count} total words")
+    print("--------- Character Count -------")
+    for entry in sort_dict(char_count):
+        if entry[0].isalpha():
+            key, value = entry[0], entry[1]
+            print(f"{key}: {value}")
+    print("============= END ===============")
 
-main()
+if len(sys.argv) == 2:
+    main()
+else:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
